688d14ef76e1e8d77d1829974bee858c8adb46b0
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 15:22:08 +0800

    Trying to deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(https);
 var port = process.env.PORT || 3000;
 var express = require('express');
 var users = [];
