da1f1043b394a001432416a15580eb7570f10271
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 14:23:27 +0800

    Fixing port error

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var port = process.env.PORT || 3000;
-var io = require('socket.io')(http);
-var express = require('express');
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-app.use(express.static("public"));
 
 http.listen(port, function () {
   console.log('listening on *: ' + port);
