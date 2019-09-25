b79ea32e6dca40c0699e71ad2811078026d465ed
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 14:08:50 +0800

    Serve index.html

---
+++ public/scripts.js
@@ -0,0 +1,93 @@
+$(function () {
+    var id;
+    var userID;
+    var listofusers = [];
+    var userName = $("#username");
+    var socket = io();
+    $("#btnLogIn").click(function () {
+      if ($("input").val() === '') {
+        alert("Fill up Username")
+      } else {
+        $("#login").fadeOut(100);
+        $("#msgs").fadeIn(100);
+        socket.emit('name entry', userName.val())//join trigger
+      }
+    });
+  
+    $('form').submit(function (e) {
+      e.preventDefault();
+      socket.emit('chat message', userName.val() + ": " + $('#m').val()); //trigger values
+      $('#m').val('');
+    });
+  
+    $("#m").on("keyup", function (e) {
+      socket.emit('typing', userName.val());
+    })
+  
+    socket.on('typing', function (msg) {
+      $("#typing").text(msg);
+      setInterval(function () {
+        $("#typing").empty()
+      }, 4000);
+    })
+  
+    socket.on('chat message', function (msg) {
+      let name = msg.split(":");
+      if (name[0] != userName.val()) {
+        var sendChat = $("<input class='form-control' style = 'float: left' readonly> <br>").val(msg)
+        $('#chatmsg').append(sendChat); //receive
+      } else {
+        var reChat = $("<input class='form-control bg-dark text-light' style = 'float: right' readonly> <br>").val(msg);
+        $('#chatmsg').append(reChat); // send
+      }
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  
+    socket.on('name entry', function (id, names) {
+      for (var i = 0; i < names.length; ++i) {
+        if (!listofusers.includes(names[i])) {
+          if (names[i] != userName.val()) {
+            listofusers.push(names[i]);
+            $("ul").append($("<li class='list-group-item text-dark' id = " + id[i] + "> " + names[i] + " has joined" + "</li>"));
+          } else {
+            $(".user").val(names[i]);
+            userID = id[i];
+          }
+        }
+      }
+      $("#ul").has("li").click(function () { // to be check
+        let liID = $("li").attr("id");
+        var message = prompt("Messages: ")
+        socket.emit('private', liID, userName.val() + ": " + message)
+      })
+    });
+  
+    
+  
+    $(window).on("beforeunload", function() {
+      alert( $(".user").val());
+      socket.emit("logout", $(".user").val());
+    })
+  
+    socket.on("logout",function(id) {
+      $("#"+id).remove();
+    })
+  
+  
+  
+    socket.on('private', function (id, msg) { // samok
+      let name = msg.split(":");
+      let names = name[0]
+      console.log("Name: " + name[0] + "Username: " + userName.val())
+      if (name[0] != userName.val()) {
+        $("#privatemsg").append($("<input class='form-control bg-dark text-light' style = 'float: left' readonly> <br>").val(msg));
+      }
+      $('#privatemsg').append($("<input class='form-control bg-primary' style = 'float: right' readonly> <br>").val(message));
+      window.scrollTo(0, document.body.scrollHeight);
+  
+    });
+  
+  
+  });
+  
+  
