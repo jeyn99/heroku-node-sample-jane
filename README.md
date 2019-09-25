32cb11f883bef73e7d7381e4e05c8547a2d4b47a
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 13:59:03 +0800

    Fixing port error

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # heroku-node-sample-jane
 A simple node app deployed to Heroku.
+
+https://heroku-node-sample-jane.herokuapp.com/ | 
+https://git.heroku.com/heroku-node-sample-jane.git
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var port = process.env.port||3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
