107ff415a63ad7004acbad312c5490be8ca52a34
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 14:02:35 +0800

    Serve index.html

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var http = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+    res.sendFile(__dirname + '/index.html');
+  });
 
 http.listen(port, function(){
   console.log('listening on *: ' + port);
