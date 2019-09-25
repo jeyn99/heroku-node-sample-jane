2440d1b22b583313485f057186997c1ee2ac5f8a
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 14:43:58 +0800

    Fix some errors

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ io.on('connection', function (socket) { // This establish connection and can che
   })
 });
 
-http.listen(port,'0.0.0.0' ,function () {
+http.listen(port, function () {
   console.log('listening on *: ' + port);
 });
