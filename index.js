c6920acea3de281342118a3471c8b42124e0e2ac
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-25 14:27:01 +0800

    Trying to deploy

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var express = require('express');
+var users = [];
+var ids = [];
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html'); // shows the complete pathname of index.html
+});
+
+app.use(express.static("public"));
+
+io.on('connection', function (socket) { // This establish connection and can check who are connected
+  socket.on('name entry', function (name) {
+    var id = socket.id;
+    if (!users.includes(name)) {
+      ids.push(id);
+      users.push(name);
+    }
+    io.emit('name entry', ids, users);
+  })
+
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('typing', function (username) {
+    socket.broadcast.emit('typing', username + " is typing...")
+  })
+
+  socket.on('private', function (id, message) {
+    io.to(id).emit('private', id, message)
+  })
+
+  socket.on("logout", function (user) {
+    var index = users.indexOf(user);
+    users.splice(index, 1);
+    var id = ids[index];
+    io.emit('logout', id);
+  })
 });
 
 http.listen(port, function () {
